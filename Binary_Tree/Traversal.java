fcd4c5927f2f27ca3e19ab0c0cf8640a980f391e
Sajid Ahmad <sajid01ahmad@gmail.com>
2025-09-13 11:58:53 +0530

    Inorder postOrder and LevelOrder Traversal

---
+++ Binary_Tree/Traversal.java
@@ -0,0 +1,74 @@
+package Binary_Tree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+import Binary_Tree.BinaryTree.BinaryTreeBt;
+import Binary_Tree.BinaryTree.Node;
+
+public class Traversal {
+
+
+    public static void preOrderTraversal(Node root){
+        if(root==null) return ;
+        System.out.println(root.data);
+        preOrderTraversal(root.left);
+        preOrderTraversal(root.right);
+    }
+
+    public static void inOrderTraversal(Node root){
+        if(root==null) return;
+        inOrderTraversal(root.left);
+        System.out.println(root.data);
+        inOrderTraversal(root.right);
+    }
+    public static void postOrderTraversal(Node root){
+        if(root==null) return;
+        postOrderTraversal(root.left);
+        postOrderTraversal(root.right);
+        System.out.println(root.data);
+    }
+
+    public static void levelOrderTraverSal(Node root){
+        if(root==null) return;
+        Queue<Node> q=new LinkedList<>();
+        q.add(root);
+        q.add(null);
+        while(!q.isEmpty()){
+            Node currNode=q.remove();
+            if(currNode==null){
+                System.out.println();
+                if(q.isEmpty()){
+                    break;
+                }else{
+                    q.add(null);
+                }
+            }else{
+                System.out.println(currNode.data + " ");
+                if(currNode.left!=null){
+                    q.add(currNode.left);
+                }if(currNode.right!=null){
+                    q.add(currNode.right);
+                }
+                
+            }
+        }
+    }
+    public static void main(String[] args) {
+    int[] nodes={1,2,4,-1,-1,5,-1,-1,3,6,-1,-1,7,-1,-1};
+
+    Node root=BinaryTreeBt.buildTree(nodes);   // did not created the object , bcz the class was static  
+    preOrderTraversal(root);
+    System.out.println();
+    inOrderTraversal(root);
+    System.out.println();
+    postOrderTraversal(root);
+    System.out.println();
+    levelOrderTraverSal(root);
+
+
+        
+    }
+
+    
+}
